99453f55fcae50c5db471a5045d40d5919fbc8b8
WasATrueWarrior <rohitjaatjaat073@gmail.com>
2024-09-09 10:30:18 +0530

    smol changes

--- src/libs/prisma.ts
+++ src/libs/prisma.ts
@@ -1,17 +1,17 @@
-//src/libs/prisma.ts
-
 import { PrismaClient } from "@prisma/client";
 
 const prismaClientSingleton = () => {
   return new PrismaClient();
 };
 
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
+declare const global: {
+  prismaGlobal?: PrismaClient;
+};
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+const prisma = global.prismaGlobal ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== "production") {
+  global.prismaGlobal = prisma;
+}
 
 export default prisma;
-
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
