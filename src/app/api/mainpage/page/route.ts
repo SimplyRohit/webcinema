703dddbce603d27698a0e37062228cf4f94e975c
SimplyRohit <rohitjaatjaat073@gmail.com>
2024-10-05 18:39:15 +0530

    final test 3.0

--- src/app/api/mainpage/page/route.ts
+++ src/app/api/mainpage/page/route.ts
@@ -5,37 +5,37 @@ const maindata = [
   {
     id: 0,
     name: "Recommendation",
-    url: `https://api.themoviedb.org/3/movie/157336/recommendations?api_key=23b2eec7e3fab51943e211619621ce2a&language=en-US&page=1`,
+    url: `https://api.themoviedb.org/3/movie/157336/recommendations?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US&page=1`,
   },
   {
     id: 1,
     name: "Latest Movies",
-    url: `https://api.themoviedb.org/3/movie/now_playing?api_key=23b2eec7e3fab51943e211619621ce2a&page=1`,
+    url: `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.NEXT_PUBLIC_API_KEY}&page=1`,
   },
   {
     id: 2,
     name: "Latest TV-Shows",
-    url: `https://api.themoviedb.org/3/tv/on_the_air?api_key=23b2eec7e3fab51943e211619621ce2a&page=1`,
+    url: `https://api.themoviedb.org/3/tv/on_the_air?api_key=${process.env.NEXT_PUBLIC_API_KEY}&page=1`,
   },
   {
     id: 3,
     name: "K-Drama Movies",
-    url: `https://api.themoviedb.org/3/discover/movie?api_key=23b2eec7e3fab51943e211619621ce2a&with_origin_country=KR&sort_by=popularity.desc&page=1`,
+    url: `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.NEXT_PUBLIC_API_KEY}&with_origin_country=KR&sort_by=popularity.desc&page=1`,
   },
   {
     id: 4,
     name: "K-Drama Shows",
-    url: `https://api.themoviedb.org/3/discover/tv?api_key=23b2eec7e3fab51943e211619621ce2a&with_origin_country=KR&sort_by=popularity.desc&page=1`,
+    url: `https://api.themoviedb.org/3/discover/tv?api_key=${process.env.NEXT_PUBLIC_API_KEY}&with_origin_country=KR&sort_by=popularity.desc&page=1`,
   },
   {
     id: 5,
     name: "Anime Movies",
-    url: `https://api.themoviedb.org/3/discover/movie?api_key=23b2eec7e3fab51943e211619621ce2a&with_genres=16&sort_by=popularity.desc&page=1`,
+    url: `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.NEXT_PUBLIC_API_KEY}&with_genres=16&sort_by=popularity.desc&page=1`,
   },
   {
     id: 6,
     name: "Anime Shows",
-    url: `https://api.themoviedb.org/3/discover/tv?api_key=23b2eec7e3fab51943e211619621ce2a&with_genres=16&sort_by=popularity.desc&page=1`,
+    url: `https://api.themoviedb.org/3/discover/tv?api_key=${process.env.NEXT_PUBLIC_API_KEY}&with_genres=16&sort_by=popularity.desc&page=1`,
   },
 ];
 
